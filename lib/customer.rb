c980d39708d55a503cc4f70a0ab846aed9b58e3c
Milan Mody <milanm1120@gmail.com>
2021-03-23 00:09:50 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,4 +13,13 @@ class Customer
     @@all
   end
 
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+  Meal.all.select do |meal|
+    meal.customer == self
+  end
+
 end
